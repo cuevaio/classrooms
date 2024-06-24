28c43ed824332dd1dbb633c1a2af380f237bb714
Anthony Cueva <hi.cuevantn@gmail.com>
2024-06-24 13:03:14 -0500

    feat: updated events up to 7/15

--- src/app/api/populate/route.ts
+++ src/app/api/populate/route.ts
@@ -8,8 +8,8 @@ import { parseEvent } from "@/lib/parseEvent";
 const xata = getXataClient();
 
 export const GET = async () => {
-  let rawStart = "15/04/2024";
-  let rawEnd = "15/06/2024";
+  let rawStart = "15/05/2024";
+  let rawEnd = "15/07/2024";
 
   let classrooms_fetched = 0;
 
